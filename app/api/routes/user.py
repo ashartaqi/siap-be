78483e72b103499c37eb9d2f89c75078a178878b
ashartaqi <ashartaqi12@gmail.com>
2026-03-19 02:08:00 +0500

    Updating user with 2 new fields and adding password validation

--- app/api/routes/user.py
+++ app/api/routes/user.py
@@ -3,25 +3,42 @@ from sqlalchemy.orm import Session
 from app.api.deps import get_db
 from app.core.security import create_access_token
 from app.crud import authenticate_user, create_user
-from app.schemas import UserLogin, UserRegister, Token
+from app.schemas import UserLogin, UserRegister, Token, RegisteredUser
 
 router = APIRouter()
 
-@router.post("/register", response_model=UserRegister)
+@router.post("/register", response_model=RegisteredUser)
 def register(user: UserRegister, db: Session = Depends(get_db)):
-    db_user = create_user(db, user.username, user.email, user.password)
-    return db_user
+    try:
+
+        db_user = create_user(db, username=user.username, email=user.email, password=user.password, first_name=user.first_name, last_name=user.last_name)
+        if not db_user:
+            raise HTTPException(status_code=400, detail="User registration failed")
+        token = create_access_token({"sub": db_user.email})
+
+        resp = RegisteredUser.model_validate(db_user)
+        resp.token = token
+        return resp
+    except HTTPException:
+        raise
+    except Exception as e:
+        print("Registration error:", str(e))
+        raise HTTPException(status_code=400, detail=str(e))
+
 
 @router.post("/login", response_model=Token)
 def login(user: UserLogin, db: Session = Depends(get_db)):
     try:
-        db_user = authenticate_user(db, user.email, user.password)
+        db_user = authenticate_user(db, email=user.email, password=user.password)
         if not db_user:
             raise HTTPException(status_code=400, detail="Incorrect username or password")
         token = create_access_token({"sub": db_user.email})
-        return {"access_token": token, "token_type": "bearer"}
+        return {"token": token, "token_type": "bearer"}
     except HTTPException:
         raise
     except Exception as e:
-        print("Login error:", e)
-        raise HTTPException(status_code=500, detail="Internal server error")
+        print("Login error:", str(e))
+        raise HTTPException(status_code=400, detail=str(e))
+
+
+
