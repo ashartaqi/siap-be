dd7c8650826dea4b48e5b2a43b57e9fcfc792727
Mustafa Faisal <mustafafaisal363@gmail.com>
2026-03-26 19:10:37 +0500

    sending list of goalkeeper based on filters to frontend in descending order

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from app.api.routes import players, teams, user
+from app.api.routes import players, teams, user, goalkeepers
 
 api_router = APIRouter()
 api_router.include_router(user.router, prefix="/user", tags=["user"])
 api_router.include_router(teams.router, prefix="/teams", tags=["teams"])
 api_router.include_router(players.router, prefix="/players", tags=["players"])
+api_router.include_router(goalkeepers.router, prefix="/goalkeepers", tags=["goalkeepers"])
